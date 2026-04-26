e29818f2d9e71ed14bff38087413c063e8ce858b
rodekedjy-source <rodekedjy@gmail.com>
2026-04-26 15:36:36 -0400

    Update ClientPage.jsx

--- organized/src/pages/ClientPage.jsx
+++ organized/src/pages/ClientPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react'
 import { supabase } from '../lib/supabase'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const LANG = {
   en: {
